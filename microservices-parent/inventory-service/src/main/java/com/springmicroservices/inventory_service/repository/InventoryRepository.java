2fda8dca69027ae249894c4cbe172ad69c557663
kiranpanwar <kiranpanwarpyds@gmail.com>
2024-12-29 00:22:43 +0530

    spring-microservices project

---
+++ microservices-parent/inventory-service/src/main/java/com/springmicroservices/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.springmicroservices.inventory_service.repository;
+
+import com.springmicroservices.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
---
+++ microservices-parent/product-service/src/main/java/com/springmicroservices/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.springmicroservices.product_service.repository;
+
+import com.springmicroservices.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
